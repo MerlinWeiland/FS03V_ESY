4a32a04726a2db643a93ca12c24f7a3c21343067
Merlin <Merlin.Weiland@outlook.com>
2024-03-20 10:19:05 +0100

    v1 menu

---
+++ menu.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+#python Porgramm
+
+
+def main():
+  print("was geht")
+  menu()
+
+def menu():
+  print("1: berechne pi")
+  print("2: berechne x quadrte")
+  num = input()
+  auswahl(num)
+
+def auswahl(value):
+  if value == 1:
+    #erstmal NOIX!
+  elif value == 2:
+    #hält die Frasse
+  else
+    pass
+
+  
+
+
+
+if __name__ == "__main__":
+  main()
+
+
